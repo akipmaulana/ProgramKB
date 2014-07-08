cdbbd41642bc194911c0cc99ff046c671c32ad90
Akip <akip.maulana@gmail.com>
2014-07-09 00:25:21 +0700

    Revisi_UI Klasifikasi
    Menjadi List

--- src/aprisma/akirah/bingung/slidingmenu/Klasifikasi.java
+++ src/aprisma/akirah/bingung/slidingmenu/Klasifikasi.java
@@ -1,17 +1,21 @@
 package aprisma.akirah.bingung.slidingmenu;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import aprisma.akirah.bingung.R;
 import aprisma.akirah.bingung.timeline.TimelineAcitivity;
 
@@ -20,27 +24,28 @@ public class Klasifikasi extends Fragment {
 
 	public final static String KLASIFIKASI_REQUEST = "klasifikasi_request";
 	public static ArrayList<String> GET_KLASIFIKASI = new ArrayList<String>();
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.klasifikasi_activity, container, false);
+		View rootView = inflater.inflate(R.layout.klasifikasi_activity,
+				container, false);
 		return rootView;
 	}
-	
+
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onViewCreated(view, savedInstanceState);
 		setKlasifikasi();
-		getKlasifikasi();
-		
+		setToList();
+
 	}
-	
+
 	/*
 	 * Untuk menyimpan Klasifikasi dari serverke dalam array GET_KLASIFIKASI
 	 */
-	private void setKlasifikasi(){
+	private void setKlasifikasi() {
 		GET_KLASIFIKASI.clear();
 		GET_KLASIFIKASI.add("Makanan");
 		GET_KLASIFIKASI.add("Jalan-jalan");
@@ -58,43 +63,94 @@ public class Klasifikasi extends Fragment {
 		GET_KLASIFIKASI.add("Munyun");
 		GET_KLASIFIKASI.add("Lainnya");
 	}
-	
+
 	/*
-	 * Untuk membuat button dari klasifikasi server
+	 * set klasifikasi ke dalam list view
 	 */
-	private void getKlasifikasi() {
+	private void setToList() {
+		ListAdapter adapter = new ListAdapter(getActivity(), GET_KLASIFIKASI);
 
-		LinearLayout klas_button = (LinearLayout) getActivity().findViewById(R.id.klasi_button);
-		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-		params.setMargins(0, 16, 0, 0); // seharusnya dinamis jangan statis
+		final ListView listView = (ListView) getActivity().findViewById(
+				R.id.list_klasifikasi);
+		listView.setAdapter(adapter);
 
-		for (int i = 0; i < GET_KLASIFIKASI.size(); i++) {
-			Button vb = new Button(getActivity());
-			vb.setBackgroundColor(0xFFF36B27); //Orange
-			vb.setTextColor(0xFFFFFFFF); //White
-			vb.setText(GET_KLASIFIKASI.get(i));
-			vb.setId(i);
-			vb.setLayoutParams(params);
-			vb.setOnClickListener(new View.OnClickListener() {
+		OnItemClickListener listener = new OnItemClickListener() {
 
-				public void onClick(View v) {
-					Boolean Found = false;
-					int indexOfsearch = 0;
-					for(String s:GET_KLASIFIKASI){
-						if (v.getId()==indexOfsearch && !Found){
-							Found = true;
-							Intent intent = new Intent(getActivity(), TimelineAcitivity.class);
-							intent.putExtra(KLASIFIKASI_REQUEST, s);
-							startActivity(intent);
-						}
-						indexOfsearch++;
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				Boolean Found = false;
+				int indexOfsearch = 0;
+				for (String s : GET_KLASIFIKASI) {
+					if (position == indexOfsearch && !Found) {
+						Found = true;
+						Intent intent = new Intent(getActivity(),
+								TimelineAcitivity.class);
+						intent.putExtra(KLASIFIKASI_REQUEST, s);
+						startActivity(intent);
 					}
+					indexOfsearch++;
 				}
-			});
 
-			klas_button.addView(vb);
+			}
+		};
 
+		listView.setOnItemClickListener(listener);
+
+	}
+
+	/*
+	 * Custom class untuk adapter list view
+	 */
+	private static class ListAdapter extends BaseAdapter {
+
+		Context ctx;
+		LayoutInflater lInflater;
+		List<String> data;
+
+		ListAdapter(Context context, List<String> data) {
+			ctx = context;
+			this.data = data;
+			lInflater = (LayoutInflater) ctx
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		}
+
+		@Override
+		public int getCount() {
+			return data.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return data.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = convertView;
+			if (view == null) {
+				view = lInflater
+						.inflate(R.layout.tv_klasifikasi, parent, false);
+			}
+
+			if (position % 2 == 0) {
+				view.setBackgroundColor(0xFFFFFFFF);
+			} else {
+				view.setBackgroundColor(0xFFF3F3F3);
+			}
+			
+			TextView tv = (TextView) view.findViewById(R.id.iniAkuTv);
+			tv.setText(data.get(position));
+
+			return view;
+		}
+
 	}
 
 }
